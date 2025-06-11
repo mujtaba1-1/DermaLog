9c81a60db0d23cbc1957481cd95a1af37bd2348e
Muhammad Mujtaba Butt <mujtabab.2006@outlook.com>
2025-06-11 17:00:27 +0100

    Added context provider to whole app

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,9 +1,12 @@
 import { Stack } from 'expo-router';
+import { LogProvider } from '../context/LogContext';
 
 export default function Layout() {
   return (
-    <Stack>
-      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-    </Stack>
+    <LogProvider>
+      <Stack>
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      </Stack>
+    </LogProvider>
   );
 }
