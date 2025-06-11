614518b2f069164e7e30620ed18f0df19c2055c6
Muhammad Mujtaba Butt <mujtabab.2006@outlook.com>
2025-06-11 16:59:53 +0100

    Added context for logs (lets me access log data on any page)

---
+++ context/LogContext.js
@@ -0,0 +1,54 @@
+import { createContext, useState, useEffect, useContext } from "react";
+import { getLogs } from "../api/logService";
+
+const LogContext = createContext();
+
+export const useLogs = () => useContext(LogContext);
+
+const groupLogsByArea = (logs) => {
+    const groups = {
+      "Hands": [],
+      "Face": [],
+      "Arms": [],
+      "Legs": [],
+      "Torso": [],
+      "Feet": []
+    };
+
+    logs.forEach(log => {
+        if (groups[log.area]) {
+            groups[log.area].push(log);
+        }
+    })
+
+    return groups;
+}
+
+export const LogProvider = ({children}) => {
+    const [logs, setLogs] = useState([]);
+    const [areaGroups, setAreaGroups] = useState({});
+    const [loading, setLoading] = useState(true);
+
+    const fetchLogs = async () => {
+        try {
+        setLoading(true);
+        const data = await getLogs();
+        setLogs(data);
+        setAreaGroups(groupLogsByArea(data));
+        } catch (err) {
+        console.error("Failed to fetch logs", err);
+        } finally {
+        setLoading(false);
+        }
+    }
+
+    useEffect(() => {
+        fetchLogs();
+    }, []);
+
+    return (
+        <LogContext.Provider value={{ logs, areaGroups, loading, refreshLogs: fetchLogs }}>
+            {children}
+        </LogContext.Provider>
+    );
+}
