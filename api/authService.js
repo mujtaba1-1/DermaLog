9ecc6ce9e9a78f6d21b9ae26b9f1000dbdfa0dec
Muhammad Mujtaba Butt <mujtabab.2006@outlook.com>
2025-06-17 18:36:47 +0100

    Added api calls to register and login

---
+++ api/authService.js
@@ -0,0 +1,21 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import api from "./apiCreation";
+
+export const login = async (userData) => {
+    try {
+        const response = await api.post('/login', userData);
+        await AsyncStorage.setItem('jwt', response.data);
+    } catch (error) {
+        console.log("Error logging in: ", error);
+        throw error;
+    }
+}
+
+export const register = async (userData) => {
+    try {
+        const response = await api.post('/register', userData)
+    } catch (error) {
+        console.log(error.response.data);
+        throw error;
+    }
+}
