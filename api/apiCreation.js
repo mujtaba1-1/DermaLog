02d382408de3117bbbdadd91f67629e5c3ac63c0
Muhammad Mujtaba Butt <mujtabab.2006@outlook.com>
2025-06-17 18:38:23 +0100

    Added interceptors to deal with token expiration and adding jwt token to the header

--- api/apiCreation.js
+++ api/apiCreation.js
@@ -1,8 +1,39 @@
-import axios from "axios";
+import axios from 'axios';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { router } from 'expo-router';
 
 const api = axios.create({
-    baseURL: 'http://192.168.0.101:8080',
-    timeout: 1000
-})
+  baseURL: 'http://192.168.0.100:8080',
+  timeout: 5000,
+});
+
+// Add a request interceptor
+api.interceptors.request.use(
+  async (config) => {
+    try {
+      const token = await AsyncStorage.getItem('jwt'); // Replace 'jwt' with your actual key
+      if (token) {
+        config.headers.Authorization = `Bearer ${token}`;
+      }
+    } catch (error) {
+      console.log('Error retrieving token:', error);
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+api.interceptors.response.use(
+    (response) => response,
+    async (error) => {
+        if (error.response && error.response.status === 403) {
+            await AsyncStorage.removeItem('jwt')
+            router.replace('/login')
+        }
+        return Promise.reject(error)
+    }
+)
 
 export default api;
